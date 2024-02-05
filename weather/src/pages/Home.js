fbaf3e4482603fad368324abab48453d68e6d190
alanren0 <68366679+alanren0@users.noreply.github.com>
2024-02-04 20:32:43 -0500

    add backlink

--- weather/src/pages/Home.js
+++ weather/src/pages/Home.js
@@ -5,6 +5,12 @@ function Home() {
   return (
     <>
         <Forecast/>
+        <footer>
+          Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a><br></br>
+          <a href="https://www.weatherapi.com/" title="Free Weather API">
+            <img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"/>
+          </a>
+        </footer>
     </>
   )
 }
